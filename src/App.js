fd15e89bc21f6d14082646991f517b7a0950e216
OsmanRana <osmancmc@yahoo.com>
2022-01-03 20:20:42 +0600

    image added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Property Rental</h1>
+      <Home/>
     </div>
   );
 }
